3e0d617ad46d7606f63c111e7c5d6f95dfd91ea6
JoshuaDodds <jdodds@local>
2025-09-18 17:27:42 +0200

    fix

--- lib/live_stream_daemon.py
+++ lib/live_stream_daemon.py
@@ -28,7 +28,7 @@ ARECORD_CMD = [
 stop_requested = False
 p = None
 
-def handle_signal(signum):
+def handle_signal(signum, frame):
     global stop_requested, p
     print(f"[live] received signal {signum}, shutting down...", flush=True)
     stop_requested = True
