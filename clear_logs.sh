151cb04f65f111936db538001f2bb68035ebe4d1
JoshuaDodds <jdodds@local>
2025-09-24 20:39:45 +0200

    fix unintended removal of all recordings

--- clear_logs.sh
+++ clear_logs.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#sudo systemctl stop systemd-journald
+sudo systemctl stop systemd-journald
 sudo journalctl --rotate
 sudo journalctl --vacuum-time=1s
 sudo rm -rf /run/log/journal/*
-#sudo systemctl start systemd-journald
-
-rm -rf /apps/tricorder/recordings/*
-rm -rf /apps/tricorder/tmp/*
+sudo systemctl start systemd-journald
